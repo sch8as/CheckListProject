390f88f148f25ac5a50095e74a2a32491b2a0677
Sch9AS <sch9as@gmail.com>
2022-08-14 13:39:35 +0400

    Adjust paddings in blades

--- resources/views/admin/check_list/index.blade.php
+++ resources/views/admin/check_list/index.blade.php
@@ -13,44 +13,42 @@
     </form>
 
     @if(count($checkLists))
-    <table class="table table-striped ">
-        <thead>
-            <tr>
-                <th scope="col">User</th>
-                <th scope="col"></th>
-                <th scope="col">List title</th>
-                <th scope="col">Element title</th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($checkLists as $checkList)
+        <table class="table table-striped ">
+            <thead>
                 <tr>
-                    <td class="fit" scope="row">
-                        <a class="btn btn-primary" href="{{ route('users.show', ['user' => $checkList->user->id]) }}">{{$checkList->user->name}}</a>
-                    </td>
-                    <td class="fit" scope="row">
-                        {{$checkList->user->email}}
-                        @if($checkList->user->status == 0)
-                            (Banned)
-                        @endif
-                    </td>
-
-                    <td class="fit" scope="row">
-                        {{$checkList->title}}
-                    </td>
-
-                    <td scope="row">
-                        @foreach($checkList->checkElements as $element)
-                            <p>{{$element->title}}</p>
-                        @endforeach
-                    </td>
+                    <th scope="col">User</th>
+                    <th scope="col"></th>
+                    <th scope="col">List title</th>
+                    <th scope="col">Element title</th>
+                    <th scope="col"></th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach($checkLists as $checkList)
+                    <tr>
+                        <td class="fit" scope="row">
+                            <a class="btn btn-primary" href="{{ route('users.show', ['user' => $checkList->user->id]) }}">{{$checkList->user->name}}</a>
+                        </td>
+                        <td class="fit" scope="row">
+                            {{$checkList->user->email}}
+                            @if($checkList->user->status == 0)
+                                (Banned)
+                            @endif
+                        </td>
 
-    {{--{{$checkLists->links()}}--}}
+                        <td class="fit" scope="row">
+                            {{$checkList->title}}
+                        </td>
+
+                        <td scope="row">
+                            @foreach($checkList->checkElements as $element)
+                                <p>{{$element->title}}</p>
+                            @endforeach
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @else
         <h4>Elements not founded</h4>
     @endif
--- resources/views/check_list/index.blade.php
+++ resources/views/check_list/index.blade.php
@@ -2,44 +2,42 @@
 
 @section('content')
     @if(count($checkLists))
-    <table class="table table-striped ">
+        <table class="table table-striped ">
 
-        <tbody>
-            @foreach($checkLists as $checkList)
-                <tr>
-                    <td class="fit" scope="row">
-                        {{$checkList->title}}
-                    </td>
+            <tbody>
+                @foreach($checkLists as $checkList)
+                    <tr>
+                        <td class="fit" scope="row">
+                            {{$checkList->title}}
+                        </td>
 
-                    <td class="fit" scope="row">
-                        <a class="btn btn-primary" href="{{ route('lists.show', ['list' => $checkList->id]) }}">Show</a>
-                    </td>
-                    <td class="fit" scope="row">
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                ☰
-                            </button>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                <li><a class="dropdown-item" href="{{ route('lists.edit', ['list' => $checkList->id]) }}">Edit</a></li>
-                                <li>
-                                    <form action="{{ route('lists.destroy', ['list' => $checkList->id]) }}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="dropdown-item" title="Delete">Delete</button>
-                                    </form>
-                                </li>
-                            </ul>
-                        </div>
-                    </td>
-                    <td scope="row">
-                        {{$checkList->description}}
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    {{--{{$checkLists->links()}}--}}
+                        <td class="fit" scope="row">
+                            <a class="btn btn-primary" href="{{ route('lists.show', ['list' => $checkList->id]) }}">Show</a>
+                        </td>
+                        <td class="fit" scope="row">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                    ☰
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li><a class="dropdown-item" href="{{ route('lists.edit', ['list' => $checkList->id]) }}">Edit</a></li>
+                                    <li>
+                                        <form action="{{ route('lists.destroy', ['list' => $checkList->id]) }}" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="dropdown-item" title="Delete">Delete</button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </div>
+                        </td>
+                        <td scope="row">
+                            {{$checkList->description}}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @else
         <h4>There has no lists</h4>
     @endif
