d6b8b7859d3998906a87ed83d3071f21fad49ae1
Sch9AS <sch9as@gmail.com>
2022-08-21 10:54:43 +0400

    Create middleware ApiCheckBanned

---
+++ app/Http/Middleware/ApiCheckBanned.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiCheckBanned
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(auth()->check() && (auth()->user()->status == User::STATUS_BANNED)){
+            //$request->session()->invalidate();
+            //$request->session()->regenerateToken();
+            return response()->json([
+                'error' => 'Your Account is suspended, please contact Admin.',
+            ], 401);
+        }
+
+        return $next($request);
+    }
+}
